7476d9377b678a486cc79c26987231f4bfbd2daa
kawlik <kpawlik1103@gmail.com>
2021-10-09 19:49:55 +0200

    Login redirect insted of render

--- controllers/auth.js
+++ controllers/auth.js
@@ -51,7 +51,7 @@ controller.success = async ( req, res, next ) => {
         // return res.status( 200 ).render( 'auth', { auth: auth, front: config.server.front });
 
         // redirects to native front server
-        return res.status( 300 ).redirect( config.server.front + auth );
+        return res.status( 300 ).redirect( config.server.front + encodeURIComponent( auth ));
 
     } catch( err ) {
 
